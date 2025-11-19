81467e80a59ad27d73207e12e10a11982d1e3996
Jake Murray-McGuirk <X00197631@mytudublin.ie>
2025-11-19 17:51:53 +0000

    User input now makes sense and events are stored in a list

---
+++ app.py
@@ -0,0 +1,18 @@
+from stats_tagger import commands, get_teams, get_event, get_outcome, get_player_no, parse_event, inputEvent
+
+match_events = []
+
+print("Enter an event in the format 'event outcome(if applicable) player no. (if applicable)\nIf outcome or player no. not available, skip that section.\nE.g. foul commited by player 14: f14\nShot wide from player 12: sw14\nStart game:sg")
+event = inputEvent()
+
+while event.strip.lower()!="end game":
+    event = event.strip.lower()
+    if event in commands:
+        print(commands[event])
+    else:
+        attempt_parse = parse_event(event)
+        if attempt_parse:
+            match_events.append(attempt_parse)
+            print(attempt_parse)
+        else:
+            print("Invalid event entered")
